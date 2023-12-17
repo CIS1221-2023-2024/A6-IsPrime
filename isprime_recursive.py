54205fdb30a07a168b8b96823cb395486213ba23
Your Name <your.email@example.com>
2023-12-17 18:22:41 +0100

    Update isprime_recursive.py
    Reviewed the prime_recursive program. Will soon upload all necessary test programs stating why I changed the functions in the way I did (includes execution time improvements as well as wider input and function possibilities/boundaries). Improved readability and less lines of code.

--- isprime_recursive.py
+++ isprime_recursive.py
@@ -1,106 +1,116 @@
-def is_prime(num, div=0): #A function to check if a number is prime
-    
-    if num <= 1:
-        return False #any number smaller or equal to 1 is not prime
-    
-    if div == 0: #div = num-1 is not allowed in parameter due to num not being instantialized, so we use this to assign div = num-1
-        div = num - 1
-    
-    if div == 1: #if no returns have happened so far, that means the number is prime
-        return True 
+# My take on the original Python Recursive program file uploaded by team 
 
-    if num % div == 0: #checks if num is perfectly divisble by div
-        return False #any number divisible by another number excluding 1 or itself is not prime
+def isPrime(n, i = 2):
+    try:
+        if (n <= 2):
+            return True if(n == 2) else False
+        if (n % i == 0):
+            return False
+        if (i**2 > n):
+            return True
+        # if n is not prime, it must have a divisor smaller than or equal to its square root
     
-    return is_prime(num, div - 1) #the function is returned again recursively with div being decremented
+    except RecursionError:
+        print("Recursion depth exceeded. Number is too large.")
+        return False
+    # Check for next divisor
+    return isPrime(n, i + 1)
 
-
-def find_all_prime_factors(n,counter = 2,List_of_primes = None): #A function that shows you all prime factors of a given number
+def prime_factors(n, counter = 2, list_of_primes = None): #shows you all prime factors of a given number
         
-     if List_of_primes == None: #this is so the list is reset per loop in the menu
-         List_of_primes = []
+     if list_of_primes == None: # list is reset per loop in the menu
+         list_of_primes = []
 
-     if counter == n+1: #Checks wether the counter has reached the number given (+1 so it includes the number)
-         return List_of_primes #returns the list of primes
+     if counter == n+1: #Checks whether the counter has reached the number given (+1 so it includes the number)
+         return list_of_primes #returns the list of primes
     
-     if n % counter == 0 and is_prime(counter): #checks wether counter is a factor of n and if its prime
-          List_of_primes.append(counter)  #adds the value of counter to the list
+     if n % counter == 0 and isPrime(counter): #checks whether counter is a factor of n and if its prime
+          list_of_primes.append(counter)  #adds the value of counter to the list
           
-     return find_all_prime_factors(n,counter +1, List_of_primes) #returns the method but with counter incremented
+     return prime_factors(n,counter +1, list_of_primes) #returns counter incremented
 
+def all_primes_up_to(n, counter=2, list_of_primes = None): #shows you a list of all prime numbers under n
 
-def all_primes_up_to(n,counter=2,List_of_primes = None): #A function that shows you a list of all prime numbers under n
+    if list_of_primes == None: # the list is reset per loop in the menu
+         list_of_primes = []
 
-    if List_of_primes == None: #this is so the list is reset per loop in the menu
-         List_of_primes = []
+    if counter == n+1: # checks whether the counter has reached the number given (+1 so it includes the number)
+         return list_of_primes #returns the list of primes
 
-    if counter == n+1: #Checks wether the counter has reached the number given (+1 so it includes the number)
-         return List_of_primes #returns the list of primes
+    if isPrime(counter) == True: # checks if the counter is prime
+         list_of_primes.append(counter)  #adds the value of the counter in the list
 
-    if is_prime(counter) == True: #checks if the counter is prime
-         List_of_primes.append(counter)  #adds the value of the counter in the list
+    return all_primes_up_to(n, counter +1, list_of_primes) #returns the method but incremen
 
-    return all_primes_up_to(n, counter +1, List_of_primes) #returns the method but increments the counter
+def nth_prime(n, counter = 2, list_of_primes = [],):   
+    
+    #checks whether there are as many values in the list as given by n
+     if len(list_of_primes) == n: 
+         return list_of_primes[n-1] # returns the last prime
 
+     #while there are less values in the list than n, continue searching
+     if len(list_of_primes) < n: 
+          if isPrime(counter) == True: # checks if the value in counter is a prime
+               list_of_primes.append(counter) #adds counter to the list
+          
+     return nth_prime(n,counter + 1) # recursive call
 
-def nth_prime(n,counter = 2, List_of_primes = [],): #A function that finds the nth prime
+def primes_in_range(start, end):
+
+    if start <= end:
+        if isPrime(start):
+            print(start)
+        primes_in_range(start + 1, end)
+        # checks if prime for start then increments until end is reached
      
-
-     if len(List_of_primes) == n: #checks wether there are as many values in the list as given by n
-         return List_of_primes[n-1] #returns the last value
-
-     if len(List_of_primes) < n: #while there are less values in the list than n
-          if is_prime(counter) == True: #checks if the value in counter is a prime
-               List_of_primes.append(counter) #adds counter to the list
-          
-     return nth_prime(n,counter + 1) 
-
-
 def menu(option = 0): 
 
-    while(option != 5): #loops until exit is chosen
-
-        option = input("\n1. Check if number is prime\n2. Find all prime factors of a number\n3. Find every prime number up to ...\n4. Find the nth prime\n5. Exit..\n") 
-        option = is_int(option)
+    while(option != 6): #loops until exit is chosen
+        print('\nEnter your choice:')
+        option = is_int(input("\n1. Check if number is prime.\n2. Find all prime factors of a number.\n3. Find every prime number up to ...\n4. Find the nth prime.\n5. Find prime numbers in range.\n6. Exit. \n")) 
 
         if option == 1: #if 1 is entered the following code is to be executed
 
-            number = input("Enter number you want to check if is prime ")
-            number = is_int(number) #checks if the number entered is an integer
-            print(is_prime(number))
+            number = is_int(input("Enter number you want to check if is prime: "))
+            print(isPrime(number))
 
-        elif option == 2: #if 2 is entered the following code is to be executed
-            number = input("Enter number you want to find its prime factors ")
-            number = is_int(number) #checks if the number entered is an integer
-            print(find_all_prime_factors(number))
+        elif option == 2: # option choice
+            number = is_int(input("Enter number to find its prime factors: "))
+            print(prime_factors(number))
             
-
-        elif option == 3: #if 3 is entered the following code is to be executed
-            number = input("Enter number you want to find the prime numbers up to ")
-            number = is_int(number) #checks if the number entered is an integer
+        elif option == 3: # option choice
+            number = is_int(input("Enter number you want to find the prime numbers up to it: "))
             print(all_primes_up_to(number))
     
-        elif option == 4: #if 4 is entered the following code is to be executed
-            number = input("Enter the nth prime ")
-            number = is_int(number) #checks if the number entered is an integer
+        elif option == 4: # option choice
+            number = is_int(input("Enter the nth position: "))
             print(nth_prime(number))
-
+            
         elif option == 5:
-            print("Exiting...") #exists the loop
+            lower_limit = is_int((input("Enter the lower limit: ")))
+            upper_limit = is_int((input("Enter the upper limit: ")))
+            
+            if(lower_limit > upper_limit):
+                print('Incorrect Values inputted.')
+                menu()
+            print(f"Prime numbers between {lower_limit} and {upper_limit}:")
+            print(primes_in_range(lower_limit, upper_limit))
+
+        elif option == 6:
+            print("Exiting program...") #exits the loop, program comes to a halt
 
         else: 
-             print("Input has to be between 1-5") #if input is not 1-5
+             print("Input has to be an integer between 1 to 6.") # boundary condition
 
+def is_int(num): # check if input number is an integer
 
-def is_int(num): #function to check if an entered number is an integer
+    while isinstance(num, int) == False:
 
-    while isinstance(num, int) == False: #checks if num is not an integer
+        try: 
+               num = int(num) # type check
 
-        try: #tries to run the followiung code
-               num = int(num)
-
-        except ValueError:    #if the error ValueError is shown, then the number is not an integer
-                print("The value you entered is not an integer, Please enter a valid integer")
+        except ValueError:    #if the error ValueError occurs, then the number is not an integer
+                print("The value you entered is not an integer, please enter a valid integer: ")
                 num = input()
     return num
 
