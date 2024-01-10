0c05af6ad4996efb228ee64acd3c74da44a969db
Shania Saliba <shania.saliba.23@um.edu.mt>
2024-01-10 09:43:51 +0100

    C# implementation + general changes

---
+++ ChallengerFiles/CSharpTests/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Prime
+{
+    public class Prime
+    {
+        public static void SieveOfEratosthenes(int n)
+        {
+            bool[] primes = new bool[n + 1];
+
+            for (int i = 0; i <= n; i++)
+                primes[i] = true;
+
+            for (int c = 2; c * c <= n; c++)
+            {
+                if (primes[c] == true)
+                {
+                    for (int i = c * c; i <= n; i += c)
+                        primes[i] = false;
+                }
+            }
+
+            Console.Write("Prime numbers up to " + n + ": ");
+            for (int i = 2; i <= n; i++)
+            {
+                if (primes[i] == true)
+                    Console.Write(i + " ");
+            }
+        }
+
+        public static void Main()
+        {
+            Console.Write("Enter number to find prime up to: ");
+            string userInput = Console.ReadLine();
+
+            // Attempt to parse the string to an integer
+            if (int.TryParse(userInput, out int userInteger))
+            {
+                Console.WriteLine($"You entered: {userInteger}");
+                Console.Write("These are the prime numbers equal or smaller to " + userInteger + ": ");
+                SieveOfEratosthenes(userInteger);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
---
+++ ChallengerFiles/CSharpTests/ReadMe.md
@@ -0,0 +1,8 @@
+Some notes: 
+While testing out the C# programs, I decided that comparing the C# algorithms was rather futile. This is since the improvements will be minimally apperant and the algorithms provided by the team were already optimized and efficient for that specific paradigm (be it iterative or recursive).
+ 
+Additionally, I would like to provide the same Primality Algorithms in C# that the team can compare their own paradigms to. For example, the implementation of the Sieve of Eratosthenes, which the team can then compare to the 'find prime numbers up to function' either in a recursive or iterative paradigm. I believe that these mathematical algorithms will further increase the realiability and complexity of the project itself.
+
+To run on VS code in integrated terminal:
+- cd to path: ...\A6-ISPRIME\ChallengerFiles\CSharpTests
+- dotnet run
